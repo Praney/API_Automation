eb77ba8ed1483b1d381463b388a272e9ef06896a
praneymadan171@gmail.com <praneymadan171@gmail.com>
2018-06-18 13:09:48 +0530

    typo

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pytest TestCalculatorService/smoketest.py -vvv --html=report.html --self-contained-html
+pytest TestCalculatorServiceFunctional/smoketest.py -vvv --html=report.html --self-contained-html
